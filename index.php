02455df366a5d83b87fad9161dcd86162eb2db67
Mike van Riel <mike@ingewikkeld.net>
2017-06-04 21:58:07 +0200

    Archive old website to a git repo

---
+++ index.php
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+  <head>
+    <title>phpDocumentor: Documentation Choices</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="keywords" content="metatag keywords" />
+    <meta http-equiv="description" content="metatag description" />
+    <link rel="shortcut icon" href="http://phpdoc.org/phpdocumentorwebsitebundle/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen, projection" title="Phpdoc" />
+  </head>
+  <body>
+    <div id="color"></div>
+    <div id="page">
+      <h2>phpDocumentor Documentation Choices</h2>
+      <h3>Frame formats</h3>
+      <div>
+        <p>Plain Html Frame formats</p>
+        <ul>
+          <li><a href="HTMLframesConverter/default/">Default</a></li>
+          <li><a href="HTMLframesConverter/earthli/">earthli</a></li>
+          <li><a href="HTMLframesConverter/l0l33t/">l0l33t</a></li>
+          <li><a href="HTMLframesConverter/phpdoc.de/">phpdoc.de</a></li>
+          <li><a href="HTMLframesConverter/phpedit/">phpedit</a></li>
+          <li><a href="HTMLframesConverter/phphtmllib/">phphtmllib</a></li>
+        </ul>
+        <p>Download versions of the above formats</p>
+        <ul>
+          <li><a href="HTMLframesConverter/default.tar.gz">Default</a></li>
+          <li><a href="HTMLframesConverter/earthli.tar.gz">earthli</a></li>
+          <li><a href="HTMLframesConverter/l0l33t.tar.gz">l0l33t</a></li>
+          <li><a href="HTMLframesConverter/phpdoc.de.tar.gz">phpdoc.de</a></li>
+          <li><a href="HTMLframesConverter/phpedit.tar.gz">phpedit</a></li>
+          <li><a href="HTMLframesConverter/phphtmllib.tar.gz">phphtmllib</a></li>
+        </ul>
+        <p>Html Frame formats with javascript enhancement</p>
+        <ul>
+          <li><a href="HTMLframesConverter/DOM/default/">Default</a></li>
+          <li><a href="HTMLframesConverter/DOM/earthli/">earthli</a></li>
+          <li><a href="HTMLframesConverter/DOM/l0l33t/">l0l33t</a></li>
+          <li><a href="HTMLframesConverter/DOM/phpdoc.de/">phpdoc.de</a></li>
+          <li><a href="HTMLframesConverter/DOM/phphtmllib/">phphtmllib</a></li>
+        </ul>
+      </div>
+      <h3>Smarty formats</h3>
+      <div>
+        <p>Html Smarty formats</p>
+        <ul>
+          <li><a href="HTMLSmartyConverter/HandS/">HandS</a></li>
+          <li><a href="HTMLSmartyConverter/PHP/">PHP</a></li>
+          <li><a href="HTMLSmartyConverter/default/">default</a></li>
+        </ul>
+        <p>Download versions of the above formats</p>
+        <ul>
+          <li><a href="HTMLSmartyConverter/HandS.tar.gz">HandS</a></li>
+          <li><a href="HTMLSmartyConverter/PHP.tar.gz">PHP</a></li>
+          <li><a href="HTMLSmartyConverter/default.tar.gz">default</a></li>
+        </ul>
+      </div>
+      <h3>PDF format</h3>
+      <div>
+        <ul>
+          <li><a href="PDFdefaultConverter/documentation.pdf">Pdf File</a></li>
+          <li><a href="PDFdefaultConverter/documentation.pdf.bz2">bz2 compressed PDF File</a></li>
+          <li><a href="PDFdefaultConverter/documentation.pdf.gz">gzipped PDF File</a></li>
+          <li><a href="PDFdefaultConverter/documentation.pdf.zip">zipped PDF File</a></li>
+        </ul>
+      </div>
+      <h3>CHM format</h3>
+      <div>
+        <ul>
+          <li><a href="CHMdefaultConverter/CHMdefaultConverter.tar.gz">gzipped archive of chm choices</a></li>
+          <li><a href="CHMdefaultConverter/documentation.chm">main chm file</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="info">
+      <h2><a href="http://phpdoc.org">Return to phpdoc.org</a></h2>
+    </div>
+    <div id="nav">
+      <div id="logo">
+        <a href="index.php" title="Home"><img src="images/logo-trans.png" alt="phpDocumentor" /></a>
+      </div>
+      <h1>phpDocumentor: Self-Created Documentation</h1>
+    </div>
+  </body>
+</html>
